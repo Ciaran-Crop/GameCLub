625170ebf43319b3aaf36bb959160e475bb91961
ciaran-crop <2297484437@qq.com>
2022-10-26 16:15:13 +0800

    delete tmpgame

--- acapp/settings.py
+++ acapp/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'superperson', 'static'),
-        os.path.join(BASE_DIR, 'tmpgame', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/bash
 
-JS_DIR=/home/acs/acapp/game/static/superperson/js
+if [ $# -lt 2 ]; then
+    echo "Usage: ./compress_game_js.sh game superperson"
+    exit 1
+fi
+JS_DIR=/home/acs/acapp/${1}/static/${2}/js
 JS_DIST=${JS_DIR}/dist
 JS_SRC=${JS_DIR}/src
 
