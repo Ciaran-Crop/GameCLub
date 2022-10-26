b074f2851c58907ef422ad47edd5463e70ba0df3
ciaran-crop <2297484437@qq.com>
2022-10-26 10:55:22 +0800

    first urls,views

--- game/views.py
+++ game/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(requset):
+    line = "<h1 style='text-align: center'>超级人类</h1>"
+    line2 = "<img src='https://ts1.cn.mm.bing.net/th/id/R-C.f73f80276a586e2683f527dde83fa56c?rik=AZ8thT0UDGw69w&riu=http%3a%2f%2fwww.guangyuanol.cn%2fuploads%2fallimg%2f210803%2f23045JE5-0-lp.jpg&ehk=xKeR1BrVBV%2fHIZdaxEO3WvKOx0EPH1t7crjj%2fp1pQAM%3d&risl=&pid=ImgRaw&r=0' style='margin: auto; display: block; width:1000px; heigh: 500px'>"
+
+    return HttpResponse(line + line2)
+
+
