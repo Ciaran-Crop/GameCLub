02c4b5a4a8ad7b287a3522d8e6ab8ca520993184
ciaran-crop <2297484437@qq.com>
2022-11-17 15:26:05 +0800

    commit apps/static

---
+++ gameclub/static/gameclub/js/src/index/back/zbase.js
@@ -0,0 +1,10 @@
+class GameClubIndexBack {
+    constructor(root){
+        this.root = root;
+        this.$back = $(`<div class="gc-index-back"></div>`)
+        this.root.$gc_index.append(this.$back);
+        this.start();
+    }
+
+    start(){}
+}
