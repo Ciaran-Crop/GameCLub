e16544a20a0964bc249ab1c4af8396600bd7b2f2
ciaran-crop <2297484437@qq.com>
2022-11-17 14:55:23 +0800

    write login page

--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -1,19 +1,27 @@
 #! /usr/bin/bash
 
-#if [ $# -lt 2 ]; then
-#    echo Usage: ${0} game superperson
-#    exit 1
-#fi
-JS_DIR=/home/acs/acapp/game/static/superperson/js
-JS_DIST=${JS_DIR}/dist
-JS_SRC=${JS_DIR}/src
+if [ $# -lt 1 ]; then
+    echo Usage: ${0} game/gameclub
+    exit 1
+fi
 
-find $JS_SRC -type f -name '*.js' | sort | xargs cat | terser -c -m > ${JS_DIST}/game.js
+case $1 in
+    "game")
+        JS_DIR=/home/acs/acapp/game/static/superperson/js
+        JS_DIST=${JS_DIR}/dist
+        JS_SRC=${JS_DIR}/src
+        find $JS_SRC -type f -name '*.js' | sort | xargs cat | terser -c -m > ${JS_DIST}/game.js
+        echo yes | python3 manage.py collectstatic
+        ;;
+    "gameclub")
+        JS_DIR=/home/acs/acapp/gameclub/static/gameclub/js
+        JS_DIST=${JS_DIR}/dist
+        JS_SRC=${JS_DIR}/src
+        echo ${JS_DIR}
 
-JS_DIR=/home/acs/acapp/gameclub/static/gameclub/js
-JS_DIST=${JS_DIR}/dist
-JS_SRC=${JS_DIR}/src
-echo ${JS_DIR}
-
-find $JS_SRC -type f -name '*.js' | sort | xargs cat | terser -c -m > ${JS_DIST}/gameclub.js
-echo yes | python3 manage.py collectstatic
+        find $JS_SRC -type f -name '*.js' | sort | xargs cat | terser -c -m > ${JS_DIST}/gameclub.js
+        echo yes | python3 manage.py collectstatic
+        ;;
+    *)
+        echo "NO~~~~~~~~~~~~~~~~~~~!!!!!!!!!!!!!!!!!!"
+esac
